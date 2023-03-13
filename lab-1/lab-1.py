aa82e1137cc98a5d2e9d61620dbeecf6a9e4d7f6
Sasha Chekh <oleksandr.chekh@chisw.com>
2023-03-13 20:57:10 +0200

    fix(lab-1): fixes according to comments

--- lab-1/lab-1.py
+++ lab-1/lab-1.py
@@ -1,18 +1,12 @@
-def writelines(line1Text, line2Text, line3Text):
-    print(line1Text)
-    print(line2Text)
+def write_lines(task_text):
+    task_name, assignee_name, mentor_name = task_text.split(',')
 
-    repeatedTeacherName = ''
-
-    for i in range(45):
-        if i == 44:
-            repeatedTeacherName += line3Text
-        else:
-            repeatedTeacherName += f'{line3Text}, '
-    print(repeatedTeacherName)
+    repeated_teacher_name = ','.join([mentor_name for _ in range(45)])
+    print(task_name)
+    print(assignee_name)
+    print(repeated_teacher_name)
 
 
-writelines("Introduction to programming: Task 1", "Sasha Chekh", "Andriy")
+write_lines('Introduction to programming: Task 1, Sasha Chekh, Andriy')
 
-
-print( ( 15 - 13.1 / 3 ) / (pow(33, 2) + 19.3) + 3.3)
+print((15 - 13.1 / 3)/(pow(33, 2) + 19.3) + 3.3)
