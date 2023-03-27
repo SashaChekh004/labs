25b01c765ac425fa79666b39a43fd53dd5ae9540
Sasha Chekh <oleksandr.chekh@chisw.com>
2023-03-27 11:49:29 +0300

    feat: add Validation

--- lab-3/Chekh_lab-3.py
+++ lab-3/Chekh_lab-3.py
@@ -1,11 +1,22 @@
 # Python programming, lab-2, Sasha Chekh
-import math
+import math as m
 
 print('Sasha Chekh')
 
+
 def calculate_math_task(x, z):
-    return x - (z / z - pow(x, 2) / 4) - pow(x, 2) / math.factorial(5)
+    return x - (z / z - pow(x, 2) / 4) - pow(x, 2) / m.factorial(5)
 
-x, z = input('Please put variables x, z for calculation: ').split()
 
-print("y = ", calculate_math_task(int(x), int(z)))
+def get_number(placeholder, allowed_type=int):
+    input_val = input(f'{placeholder} ')
+    if allowed_type(input_val):
+        return allowed_type(input_val)
+    raise Exception(f'Please input {allowed_type.__name__}')
+
+
+x = get_number('Please input integer x: ')
+z = get_number('Please input integer z: ')
+
+
+print('y = ', calculate_math_task(int(x), int(z)))
