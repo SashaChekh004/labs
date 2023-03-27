61d11bf55ef94c11ad19561b4c88e4e6cf174cca
Sasha Chekh <oleksandr.chekh@chisw.com>
2023-03-27 11:56:34 +0300

    feat: add Validation

--- lab2/Chekh_lab-2.py
+++ lab2/Chekh_lab-2.py
@@ -1,8 +1,22 @@
 # Python programming, lab-2, Sasha Chekh
 print('Sasha Chekh')
+
+
 def calculate_math_task(x, y, z):
     return ((15 - x / y) / pow(33, y) + 19.3) + z
 
-x, y, z = input('Please put variables x, y, z for calculation: ').split()
 
-print(calculate_math_task(int(x), int(y), int(z)))
+def get_number(var_name, allowed_type=int):
+    input_val = input(f'Please put variable {var_name} for calculation: ')
+    try:
+        converted_value = allowed_type(input_val)
+        return converted_value
+    except ValueError:
+        print(f'Please input {allowed_type.__name__}')
+
+
+x = get_number('x')
+y = get_number('y')
+z = get_number('z')
+
+print(calculate_math_task(x, y, z))
